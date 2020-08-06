4fd486d4e35569d62a6e569f23cf63bdc727af12
Crowdbotics <team@crowdbotics.com>
2020-08-06 18:17:54 +0000

    Update admin title

--- backend/my_app_19269/urls.py
+++ backend/my_app_19269/urls.py
@@ -32,9 +32,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "My App"
-admin.site.site_title = "My App Admin Portal"
-admin.site.index_title = "My App Admin"
+admin.site.site_header = "Sendoff"
+admin.site.site_title = "Sendoff Admin Portal"
+admin.site.index_title = "Sendoff Admin"
 
 # swagger
 schema_view = get_schema_view(
