692ad86f0ec32acc6b4eb07e35397f608d293697
Crowdbotics <team@crowdbotics.com>
2020-08-09 15:21:08 +0000

    remove package blueprint-react-native-addItem-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -8,7 +8,6 @@ export const installed_blueprints = [
 { name: 'Maps88765', human_name: 'Maps', access_route: 'Maps88765', icon: 'map'},
 { name: 'UserProfile88761', human_name: 'User Profile', access_route: 'UserProfile88761'},
 { name: 'Maps88717', human_name: 'Maps', access_route: 'Maps88717', icon: 'map'},
-{ name: 'Add-Item88716', human_name: 'Add-item', access_route: 'Add-Item88716'},
 { name: 'Maps88712', human_name: 'Maps', access_route: 'Maps88712', icon: 'map'},
 { name: 'BlankScreen6288683', human_name: 'Blank Screen 62', access_route: 'BlankScreen6288683'},
 { name: 'BlankScreen6188682', human_name: 'Blank Screen 61', access_route: 'BlankScreen6188682'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -13,7 +13,6 @@ import Add-Item88769Navigator from '../features/Add-Item88769/navigator';
 import Maps88765Navigator from '../features/Maps88765/navigator';
 import UserProfile88761Navigator from '../features/UserProfile88761/navigator';
 import Maps88717Navigator from '../features/Maps88717/navigator';
-import Add-Item88716Navigator from '../features/Add-Item88716/navigator';
 import BlankScreen6288683Navigator from '../features/BlankScreen6288683/navigator';
 import BlankScreen6188682Navigator from '../features/BlankScreen6188682/navigator';
 import BlankScreen6088681Navigator from '../features/BlankScreen6088681/navigator';
@@ -37,7 +36,6 @@ Add-Item88769: { screen: Add-Item88769Navigator },
 Maps88765: { screen: Maps88765Navigator },
 UserProfile88761: { screen: UserProfile88761Navigator },
 Maps88717: { screen: Maps88717Navigator },
-Add-Item88716: { screen: Add-Item88716Navigator },
 BlankScreen6288683: { screen: BlankScreen6288683Navigator },
 BlankScreen6188682: { screen: BlankScreen6188682Navigator },
 BlankScreen6088681: { screen: BlankScreen6088681Navigator },
